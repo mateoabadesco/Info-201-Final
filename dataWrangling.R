1c5acc18e2e5a8cbe5c0740deb27d8ec14731dcb
Gungilr <68489659+Gungilr@users.noreply.github.com>
2024-02-18 21:33:54 -0800

    Add files via upload

--- dataWrangling.R
+++ dataWrangling.R
@@ -3,10 +3,15 @@ library("ggplot2")
 
 setwd("C:\\Users\\4nime.DESKTOP-MQ9FBUC\\OneDrive\\Desktop\\school\\info 201\\")
 
-homelessness_df <- read.csv("2023-HIC-Counts-by-State.csv")
+homelessness_2023_df <- read.csv("2023-HIC-Counts-by-State.csv")
+homelessness_2022_df <- read.csv("2022-HIC-Counts-by-State.csv")
+homelessness_2021_df <- read.csv("2021-HIC-Counts-by-State.csv")
 property_df <- read.csv("HPI_master.csv")
 
 # create new var based on change in population per shelter based on growth in housing costs
 
-PIT_df <- homelessness_df %>% group_by(CocState) %>% summarize(PIT = sum(PIT.Count, na.rm = TRUE)) 
+homelessness_df <- merge(homelessness_2021_df, homelessness_2022_df, all = TRUE)
+homelessness_df <- merge(homelessness_df, homelessness_2023_df, all =  TRUE)
 
+PIT_df <- homelessness_df %>% group_by(CocState) %>% summarize(total_estimate = sum(PIT.Count, na.rm = TRUE)) 
+PIT_df <- PIT_df %>% mutate(year = year) 
