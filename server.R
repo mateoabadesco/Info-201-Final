9bc151b76e87c300516f69e4ae6b5e9eebf24751
Gungilr <4nimefan@gmail.com>
2024-03-04 14:18:34 -0800

    merge

--- server.R
+++ server.R
@@ -10,46 +10,10 @@ library(maps)
 library(mapdata)
 
 setwd("C:\\Users\\4nime\\Desktop\\School\\Info 201\\Info-201-Final")
-homelessness_2023_df <- read.csv("2023-HIC-Counts-by-State.csv")
-homelessness_2022_df <- read.csv("2022-HIC-Counts-by-State.csv")
-homelessness_2021_df <- read.csv("2021-HIC-Counts-by-State.csv")
-property_df <- read.csv("HPI_master.csv")
 state_name_abv_df <- data.frame(State.Code = state.abb, State.Name = state.name)
-
-# create new var based on change in population per shelter based on growth in housing costs
-
-homelessness_df <- merge(homelessness_2021_df, homelessness_2022_df, all = TRUE)
-homelessness_df <- merge(homelessness_df, homelessness_2023_df, all =  TRUE)
-
-homelessness_df$yearState <- paste(homelessness_df$year, homelessness_df$CocState, sep = " ")
-
-PIT_df <- homelessness_df %>% group_by(yearState) %>% summarize(PIT = sum(PIT.Count, na.rm = TRUE)) 
-
-
-property_df <- property_df %>% filter(level == "State") %>% filter(yr > 2020)
-property_df$yearState <- paste(property_df$yr, property_df$place_id, sep = " ")
-
-Price_df <- property_df %>% group_by(yearState) %>% summarize(costIndex = mean(index_nsa, na.rm = TRUE)) 
-
-joined_df <- left_join(Price_df, PIT_df, by = "yearState")
-
-column_names <- colnames(joined_df)
-print(column_names)
-
-# Data cleaning
-# Check for missing values in each column
-apply(joined_df, 2, function(x) any(is.na(x)))
-# From our output, we have no missing values
-# Lets create a new categorical variable by Separating YearState Column
-joined_df <- separate(joined_df, yearState, into = c("year", "state"), sep = " ")
-
-
-
-
 
 server <- function(input, output) {
 
-  setwd("C:\\Users\\4nime\\Desktop\\School\\Info 201\\Info-201-Final")
 homelessness_2023_df <- read.csv("2023-HIC-Counts-by-State.csv")
 homelessness_2022_df <- read.csv("2022-HIC-Counts-by-State.csv")
 homelessness_2021_df <- read.csv("2021-HIC-Counts-by-State.csv")
@@ -94,14 +58,16 @@ joined_df <- separate(joined_df, yearState, into = c("year", "state"), sep = " "
     state <- left_join(state, state_name_abv_df, by = c("region" = "State.Name"))
     state <- left_join(state, filtered_df, by = c("State.Code" = "state"))
     
-    #print(state)
+    
+    
+    print(n = 49, state %>% group_by(region) %>% summarize(stat = first(PIT/costIndex)))
     state_plot <- ggplot(data=state, aes(x=long, y=lat, fill=PIT/costIndex, group=group)) + 
       geom_polygon(color = "white") + 
       scale_fill_gradient(low = "lightblue", high = "darkblue") +
       guides(fill=FALSE) + 
       theme(axis.title.x=element_blank(), axis.text.x=element_blank(), axis.ticks.x=element_blank(),
             axis.title.y=element_blank(), axis.text.y=element_blank(), axis.ticks.y=element_blank()) + 
-      ggtitle('U.S. Map with States') + 
+      ggtitle('U.S. Map with States Homlessness to housing index') + 
       coord_fixed(1.3)
     
 
